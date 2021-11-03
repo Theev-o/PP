d3ad5e8cf012b0b69f94d60a82706785dbdddadd
theev-o <dsw46651@student.dsw.edu.pl>
2021-11-03 15:21:19 +0100

    Commented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,7 @@ public class Main {
     }
 
     static void zad2(){
-
+        //W tym zadaniu iterujemy po pytaniach, odpowiedziach i "elementach wypowiedzi". Proste pętle.
         String[] questions = {"Name: ", "Age: ", "School: ", "Major: ", "Favorite subject: ", "Least favorite subject: "
                 , "Favorite food: ", "Favorite drink: "};
         String[] answers = new String[questions.length];
@@ -36,7 +36,7 @@ public class Main {
         int[] numbers = new int[3];
         int i = 1;
         int max;
-
+        //Przypisanie kolejnych liczb do tablicy. Mogłem użyć pętli, nie chciało mi się.
         System.out.println("First number:");
         numbers[0] = input.nextInt();
         System.out.println("Second number:");
@@ -44,6 +44,7 @@ public class Main {
         System.out.println("Third number:");
         numbers[2] = input.nextInt();
 
+        //Przechodzimy przez tablicę, zapamiętując największą liczbę.
         for (max = numbers[0]; i < 3; i++) {
             if (max < numbers[i]){
                 max = numbers[i];
@@ -57,7 +58,7 @@ public class Main {
     static void zad4(){
 
         int[] numbers = new int[5];
-
+        //Tutaj powinienembył użyć pętli, nie użyłem.
         System.out.println("First number:");
         numbers[0] = input.nextInt();
         System.out.println("Second number:");
@@ -69,13 +70,16 @@ public class Main {
         System.out.println("Fifth number:");
         numbers[4] = input.nextInt();
 
+        //Szybka implementacja insert sorta:
         for(int i = 0; i < 4; i++){
-            int tmp;
-            int j = i;
-            while( j >= 0 && numbers[j+1] < numbers[j]){
+            int tmp;    //"cache" do zamiany liczb miejscami
+            int j = i;  //Iterator, który będzie przechodził po kolejnych pozycjach tablicy
+            while( j >= 0 && numbers[j+1] < numbers[j]){ //Jeżeli liczba w danym miejscu jest mniejsza od następnej...
+                //...Zamieniamy je miejscami...
                 tmp = numbers[j];
                 numbers[j] = numbers[j+1];
                 numbers[j+1] = tmp;
+                //...I sprawdzamy znowu dla każdej ppoprzedniej pary liczb
                 j--;
             }
         }
@@ -86,13 +90,14 @@ public class Main {
     }
 
     static void zad5(float R){
+        //Wiele tłumaczyć nie trzeba :)
         System.out.println("Area: " + (4.0f * Math.PI * Math.pow(R, 2)));
         System.out.println("Volume: " + ((4.0f/3.0f) * Math.PI * Math.pow(R, 3)));
     }
 
     static void zad6(float price, int n){
         float installment = 0.0f;
-
+        //Proste wyrażenia warunkowe do sprawdzenia zakresów, proste operacje na liczbach
         if(n >= 1 && n <=12){
             System.out.println("3% interest");
             installment = (price / (float)n) * 1.03f;
@@ -110,11 +115,12 @@ public class Main {
     static void zad7(){
         Double a, b;
         Character operator;
-
+        //Każdy element działania jest oddzielnym wejściem od użytkownika
         System.out.println("Please input equation. Separate numbers and operator by pressing enter.");
         a = input.nextDouble();
         operator = input.next().charAt(0);
         b = input.nextDouble();
+        //Sprawdzam, czy operator istnieje, wykonuję działanie
         switch (operator){
             case '+':
                 System.out.println(" = " + (double)(a + b));
@@ -138,25 +144,34 @@ public class Main {
     }
 
     public static void zad8(int n){
+        //Podejście iteracyjne dla zdecydowanie większej prędkości i wydajności w pamięci
         long a = 1, b = 1, cache;
+        //Pierwsze dwie liczby zawsze takie same
         System.out.println(a);
         System.out.println(b);
         for(int i = 0; i < n; i++){
+            //Cache tymczasowo przechowuje starą wartość
             cache = b;
+            //Wypisujemy kolejną liczbę
             System.out.println(a+b);
+            //Nowa wartość b
             b += a;
+            //Stare b przypisujemy a
             a = cache;
         }
     }
 
     static class zad9{
+        //Klasa stworzona dla większej czytelnośći.
         public static void insertSort(int[] numbers){
-            System.out.print("Input: [");
+            //Informacja dla użytkownika
+            System.out.print("\nInput: [");
             for(int number : numbers){
                 System.out.print(" " + number);
             }
-
             System.out.print("].");
+
+            //Ten sam insertion sort co poprzednio
             for(int i = 0; i < numbers.length - 1; i++){
                 int tmp;
                 int j = i;
@@ -168,21 +183,27 @@ public class Main {
                 }
             }
 
+            //Znowu informacja dla użytkownika
             System.out.print("\nOutput sorted by insert sort: [");
             for(int number : numbers){
                 System.out.print(" " + number);
             }
-            System.out.print("].");
+            System.out.print(" ].\n");
         }
 
         public static void bubbleSort(int[] numbers){
+            //upperBound ogranicza długość wykonywania jednej z pętli
             int upperBound = numbers.length-1;
             int cache;
-            System.out.print("Input: [");
+            //Informacje dla użytkownika
+            System.out.print("\nInput: [");
             for(int number : numbers){
                 System.out.print(" " + number);
             }
+            System.out.print("].\n");
+            //Dopóki nie posortujemy całej tablicy...
             while(upperBound > 0) {
+                //...Porównujemy każdą liczbę z następną, zamieniamy jeżeli jest większa
                 for (int i = 0; i < upperBound; i++) {
                     if (numbers[i] > numbers[i + 1]) {
                         cache = numbers[i + 1];
@@ -190,39 +211,51 @@ public class Main {
                         numbers[i] = cache;
                     }
                 }
+                //Po każdym przejściu kolejna liczba na końcu jest posortowana, zostanie zignorowana przy kolejnych
                 upperBound--;
             }
 
-            System.out.print("\nOutput sorted by bubblesort: [");
+            //Informacje dla użytkownika
+            System.out.print("Output sorted by bubblesort: [");
             for(int number : numbers){
                 System.out.print(" " + number);
             }
-            System.out.print("].");
+            System.out.print(" ].\n");
         }
 
         public static int[] mergeSort(int[] numbers){
+            //Podejście rekurencyjne, tak dla rozmaitości
             if(numbers.length == 1){
+                //Jeżeli pozostała jedna liczba, zwracamy - warunek STOP-u
                 return numbers;
             } else {
+                //Wywołujemy funkcję mergeSort ponownie dla podtablic będących lewą i prawą połówką głównej tablicy
                 int[] left = mergeSort(Arrays.copyOfRange(numbers, 0, numbers.length/2));
                 int[] right = mergeSort(Arrays.copyOfRange(numbers, numbers.length/2, numbers.length));
+                //Zwracamy połączone tablice z tych wywołań
                 return merge(left, right);
             }
         }
 
         public static int[] merge(int[] a, int[] b){
+            //Tutaj nieco kodu ze stackoverflow (obiecuję, że tylko te dwie linie) - prymitywna tablica do listy Integer
             LinkedList<Integer> listA = new LinkedList<Integer>(IntStream.of(a).boxed().collect(Collectors.toList()));
             LinkedList<Integer> listB = new LinkedList<Integer>(IntStream.of(b).boxed().collect(Collectors.toList()));
+            //Tablica do zwrócenia, jej długość jest równa długości podtablic
             int[] mergedArray = new int[a.length + b.length];
             int i = 0;
+            //Iterujemy aż jedna z podlist nie będzie pusta
             while(listA.size() != 0 && listB.size() != 0){
+                //Mniejszą z wartości "wkładamy" do tablicy do zwrócenia, usuwamy z listy, z której pochodzi
                 if(listA.element() < listB.element()){
                     mergedArray[i] = listA.poll();
                 } else {
                     mergedArray[i] = listB.poll();
                 }
+                //Iterujemy dalej po tablicy do zwrócenia
                 i++;
             }
+            //Kiedy jedna z podlist jest pusta, zawartość drugiej podlisty "wkładamy" do tablicy do zwrócenia
             if(listA.size() == 0){
                 while(listB.size() != 0){
                     mergedArray[i] = listB.poll();
@@ -238,10 +271,32 @@ public class Main {
         }
     }
     public static void main(String[] args) {
+
+//        zad1();
+//        zad2();
+//        zad3();
+//        zad4();
+//        zad5(2.34f);
+//        zad6(2999.99f, 24);
+//        zad7();
+//        zad8(75);
+
+        /*
         int[] numbers = {1, 5, 7, 3, 8, 4, 9, 0, 2, 6, 34, 72, 24, 63, 12,75, 22, 24,12};
+
+        zad9.insertSort(numbers);
+        zad9.bubbleSort(numbers);
+
+        System.out.print("\nInput: [");
+        for(int number : numbers){
+            System.out.print(" " + number);
+        }
         int[] sortedNumbers = zad9.mergeSort(numbers);
+        System.out.print("\nOutput sorted by merge sort: [ ");
         for (int number : sortedNumbers){
             System.out.print(number + " ");
         }
+        System.out.print("].");
+        */
     }
 }
